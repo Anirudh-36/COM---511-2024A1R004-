f604a1e5decb17b34f9f219d0ab455830b031be5
Anirudh <2024a1r004@mietjammu.in>
2026-09-16 11:52:22 +0530

    fifth commit

---
+++ exp3.py
@@ -0,0 +1,8 @@
+mydict = {
+    "Name" : "Anirudh",
+    "Subject" : "Python Programming",
+    "Course" : "B.Tech CSE"
+}
+print("Element of dictionary :")
+for key,value in mydict.items():
+    print(key, ":",value)
